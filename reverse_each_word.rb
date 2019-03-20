cf9edff32eac191e809f7ecfd0438d4f81e2fc4f
SOYEONG OH <axionoso@gmail.com>
2019-03-20 16:24:14 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,18 @@
 def reverse_each_word(string)
-  # str = string.split(" ")
-  # reverse = []
-  # str.each do |word|
-  #   reverse << word.reverse
+  # # str = string.split(" ")
+  # # reverse = []
+  # # str.each do |word|
+  # #   reverse << word.reverse
+  # # end
+  # # return reverse.join(" ")
+  #
+  # reversed = string.split(" ").collect do |word|
+  #   word.reverse
   # end
-  # return reverse.join(" ")
+  # reversed.join(" ")
 
   reversed = string.split(" ").collect do |word|
     word.reverse
   end
-  reversed.join(" ")
+  return reversed.join(" ")
 end
