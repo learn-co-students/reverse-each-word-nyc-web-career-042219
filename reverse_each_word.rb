b27079f14562e47e2f30e396b7e773f30e1cf96a
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-08 22:38:54 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(sentence)
-  nu_sentence = []
-  #sentence.reverse_each { |v| p v }
+  #nu_sentence = []
   sentence.split(" ") = nu_sentence
+  #sentence.reverse_each { |v| p v }
   
 end
