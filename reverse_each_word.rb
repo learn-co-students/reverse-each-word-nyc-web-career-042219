fc7b1070bd61a1ae982c9299225580f763c5fa32
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-09 03:04:02 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,18 @@
 
-def reverse_each_word(sentence)
-  #nu_sentence = []
-  sentence.split(" ") = nu_sentence
-  #sentence.reverse_each { |v| p v }
-  
+def reverse_each_word(string)
+  sentence = string.split(" ")
+  return_array = []
+  sentence.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  sentence = string.split(" ")
+  test_sentence = []
+  sentence.collect do|string| 
+    test_sentence << string.reverse
+  end
+  test_sentence.join(" ")
 end
